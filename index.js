92324775a079e5599f709d617186ffd2529ec63a
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-03-31 18:01:00 +0530

    modal popup added

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// 
-
-
 function validate() {
 
     let email_id = "admin@database.com";
     let password = "donotshare";
-
-
-
-
-
 
     let user_mail_id = document.getElementById("inputEmail").value;
     let user_password = document.getElementById("inputPassword").value;
@@ -18,8 +10,21 @@ function validate() {
         document.getElementById("loginForm").submit();
     } else {
 
-        document.getElementById("error").innerText = "Incorrect Username / Password";
+        // document.getElementById("error").innerText = "Incorrect Username / Password";
+        // window.confirm("Invalid User");
+        document.getElementById("myModal").style.display = "block";
     }
 
 
+}
+
+
+document.getElementById("close_1").onclick = function() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
 }
